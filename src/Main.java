58fae5a1280116367b3abe73819d51b7aa72fab1
Mateus Duarte <mateusrduarte@gmail.com>
2024-11-22 16:22:58 -0300

    Criada a classe Movie e seus respectivos métodos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Movie movie = new Movie();
+
+        movie.title = "The Lord of the Rings";
+        movie.year = 2001;
+        movie.director = "Peter Jackson";
+        movie.durationInMinutes = 178;
+
+        movie.showTechnicalSheet();
+        movie.rate(8);
+        movie.rate(5);
+        movie.rate(10);
+
+        System.out.println(movie.averageRating());
     }
 }
