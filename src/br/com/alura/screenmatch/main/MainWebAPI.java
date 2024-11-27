b09643c1ff9a15d9b46750da0277923437d3c2fd
Mateus Duarte <mateusrduarte@gmail.com>
2024-11-27 15:09:59 -0300

    Add movie title list and save to JSON file
    Implemented a list to store the fetched movie titles and added functionality to write this list to a JSON file named `movies-list.json`. Updated the search loop to continue until "sair" is entered.

--- src/br/com/alura/screenmatch/main/MainWebAPI.java
+++ src/br/com/alura/screenmatch/main/MainWebAPI.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -14,48 +15,72 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainWebAPI {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        List<Title> titleList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um filme para buscar:");
-        var search = sc.nextLine();
+        String search = "";
 
-        String encodedSearch = URLEncoder.encode(search, StandardCharsets.UTF_8);
-
-        String url = "https://www.omdbapi.com/?t=" + encodedSearch + "&apikey=bb69bdfd";
-        try {
-
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .build();
-
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-
-            String json = response.body();
-            System.out.println(json);
-
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
-
-            TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(tituloOmdb);
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
 
-            Title myTitle = new Title(tituloOmdb);
-            System.out.println(myTitle);
-        } catch (NumberFormatException e) {
-            System.out.println("Aconteceu um erro: ");
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Algum erro de argumento na busca, verifique o endereço");
-        } catch (YearConversationErrorException e) {
-            System.out.println(e.getMessage());
+        while (!search.equalsIgnoreCase("sair")) {
+            System.out.println("Digite um filme para buscar:");
+            search = sc.nextLine();
+
+            if (search.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+
+            String encodedSearch = URLEncoder.encode(search, StandardCharsets.UTF_8);
+
+            String url = "https://www.omdbapi.com/?t=" + encodedSearch + "&apikey=bb69bdfd";
+            System.out.println(url);
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(url))
+                        .build();
+
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(tituloOmdb);
+
+
+                Title myTitle = new Title(tituloOmdb);
+                titleList.add(myTitle);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de argumento na busca, verifique o endereço");
+
+            } catch (YearConversationErrorException e) {
+                System.out.println(e.getMessage());
+            }
         }
+
+        System.out.println(titleList);
+
+        FileWriter written = new FileWriter("movies-list.json");
+        written.write(gson.toJson(titleList));
+        written.close();
     }
 }
